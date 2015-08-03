3111e0de5c9a843db5df080076c805da451990f0
MemoRyAxis <memoryaxis@gmail.com>
2015-08-03 18:48:01 +0800

    1. jar resources

--- src/main/java/com/memory/Application.java
+++ src/main/java/com/memory/Application.java
@@ -1,5 +1,10 @@
 package com.memory;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Properties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,8 +14,26 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
+  public static void main(String[] args) throws IOException {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    System.out.println("\t-> test url1");
+    URL url1 = Application.class.getClassLoader().getResource("/application.properties");
+    System.out.println(url1); // null
+
+    System.out.println("\t-> test is1");
+    InputStream is1 = Application.class.getResourceAsStream("/application.properties");
+    System.out.println(is1); // jar:file:/E:/workspace0319/spring-boot/ta................
+    Properties prop = new Properties();
+    prop.load(is1);
+    System.out.println(prop.get("spring.redis.host"));
+
+    System.out.println("\t-> test url2");
+    URL url2 = Application.class.getResource("/application.properties");
+    System.out.println(url2); // file:E:.................
+    prop = new Properties();
+    prop.load(url2.openStream());
+    System.out.println(prop.get("spring.redis.host"));
+
+    SpringApplication.run(Application.class, args);
+  }
 }
