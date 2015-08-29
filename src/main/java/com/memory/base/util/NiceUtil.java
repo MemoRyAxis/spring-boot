924cca168aa9f4135c4923bc8ee776cc4b1143f1
memoryaxis <memoryaxis@gmail.com>
2015-08-30 02:34:04 +0800

    1. map2object

--- src/main/java/com/memory/base/util/NiceUtil.java
+++ src/main/java/com/memory/base/util/NiceUtil.java
@@ -1,30 +1,73 @@
 package com.memory.base.util;
 
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 public class NiceUtil {
 
-  private static long autoIncrease = 1;
+    private static long autoIncrease = 1;
 
-  public static long getIncreaseLong() {
-    return autoIncrease++;
-  }
+    public static long getIncreaseLong() {
+        return autoIncrease++;
+    }
 
-  public static void dissect(Object obj) {
-    if (obj == null) {
-      System.out.println("obj : null");
-      return;
+    public static void dissect(Object obj) {
+        if (obj == null) {
+            System.out.println("obj : null");
+            return;
+        }
+        Class<?> clazz = obj.getClass();
+        Field[] fields = clazz.getDeclaredFields();
+        try {
+            for (Field field : fields) {
+                field.setAccessible(true);
+                System.out.println(field.getName() + " : " + field.get(obj));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    @SuppressWarnings("rawtypes")
-    Class clazz = obj.getClass();
-    Field[] fields = clazz.getDeclaredFields();
-    try {
-      for (Field field : fields) {
-        field.setAccessible(true);
-        System.out.println(field.getName() + " : " + field.get(obj));
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
+
+    public static <T> T map2obj(Map<String, Object> map, Class<T> clazz) throws Exception {
+        if (map == null || clazz == null) {
+            return null;
+        }
+        T t = clazz.newInstance();
+        if (map.isEmpty()) {
+            return t;
+        }
+        BeanInfo beanInfo = Introspector.getBeanInfo(clazz);
+        PropertyDescriptor[] descriptors = beanInfo.getPropertyDescriptors();
+        for (PropertyDescriptor descriptor : descriptors) {
+            String fieldName = descriptor.getName();
+            if (!fieldName.equals("class") && map.containsKey(fieldName)) {
+                Method setter = descriptor.getWriteMethod();
+                setter.invoke(t, map.get(fieldName));
+            }
+        }
+        return t;
     }
-  }
+
+    public static Map<String, Object> obj2map(Object obj) {
+        if (obj == null) {
+            return null;
+        }
+        Map<String, Object> map = new HashMap<String, Object>();
+        Class<?> clazz = obj.getClass();
+        Field[] fields = clazz.getDeclaredFields();
+        try {
+            for (Field field : fields) {
+                field.setAccessible(true);
+                map.put(field.getName(), field.get(obj));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
 }
